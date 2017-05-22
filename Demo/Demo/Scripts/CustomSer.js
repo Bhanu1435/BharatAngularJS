65317bf3e9ca22c28e96912215f9b9f34d366d0c
Bhanu1435 <bharat.masters@gmail.com>
2017-05-22 10:17:05 -0700

    Add files via upload

---
+++ Demo/Demo/Scripts/CustomSer.js
@@ -0,0 +1,14 @@
+﻿//We can make service in controller by providing logic functions but the problem that 
+//we will face is that , for using the same function logic we have to copy this code again, 
+//thats the area where service and factory comes into picture
+
+
+var myApp = angular
+    .module("myModule", [])
+    .controller("myServiceController", function ($scope, OutputFactory) {
+        //$scope.message = "Angular JS";
+        $scope.transformString = function (input) {
+            $scope.output = OutputFactory.processString(input);
+        }
+
+    });
---
+++ Demo/Demo/Scripts/Script.js
@@ -0,0 +1,63 @@
+﻿//Docs BY @BHARATSARASWAT
+//shortcut method like post,put, delete are also available with shortcut methods
+//Service in Angular JS is nothing but an object that provide some sort of service that can be reused within an angular application.
+
+//we cant use by making a scope variable for getting http.get as it returns promise object
+// $scope.employees = $http.get('EmployeeService.asmx/GetAllEmployees')
+//Iinstead use then function
+
+
+/*
+//Simple Calling a webservice and populating that data on HTML @bharat
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope, $http) {
+    //$scope.message = "Angular JS";
+        $http.get('EmployeeService.asmx/GetAllEmployees')
+        .then(function (response) {
+            $scope.employees = response.data;
+            })
+            .catch(function (response) {
+                console.error('Gists error', response.status, response.data);
+            });
+});
+
+*/
+//Another way of calling the Web Services using "http Service"
+/*
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope, $http) {
+        //$scope.message = "Angular JS";
+        $http({
+            method:'GET',
+            url: 'EmployeeService.asmx/GetAllEmployees'})
+            .then(function (response) {
+                $scope.employees = response.data;
+            })
+            .catch(function (response) {
+                console.error('Gists error', response.status, response.data);
+            });
+    });
+*/
+//We can also $logService to log the response object to the console
+//if service fails we can see the error on html, as i did in div tag, and also we can log an error to console
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope, $http,$log) {
+        //$scope.message = "Angular JS";
+        $http({
+            method: 'GET',
+            url: 'EmployeeService.asmx/GetAllEmployees'
+        })
+            .then(function (response) {
+                $scope.employees = response.data;
+                $log.info(response);
+            }, function (reason) {
+                $scope.error = reason.data;
+                $log.info(reason);
+            });
+
+    });
+
+
